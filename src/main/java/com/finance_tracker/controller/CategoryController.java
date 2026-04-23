f606aff73d1d990089b44377753221674985b48e
Kyrylo Klochek <listerstellar@gmail.com>
2026-04-23 17:43:43 +0200

    Add Category service and controller

---
+++ src/main/java/com/finance_tracker/controller/CategoryController.java
@@ -0,0 +1,25 @@
+package com.finance_tracker.controller;
+
+import com.finance_tracker.entity.Category;
+import com.finance_tracker.service.CategoryService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/categories")
+@RequiredArgsConstructor
+public class CategoryController {
+    private final CategoryService categoryService;
+
+    @GetMapping
+    public List<Category> getAllCategories() {
+        return categoryService.getAllCategories();
+    }
+
+    @PostMapping
+    public Category createCategory(@RequestBody Category category) {
+        return categoryService.createCategory(category);
+    }
+}
---
+++ src/main/java/com/finance_tracker/service/CategoryService.java
@@ -0,0 +1,22 @@
+package com.finance_tracker.service;
+
+import com.finance_tracker.entity.Category;
+import com.finance_tracker.repository.CategoryRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class CategoryService {
+    private final CategoryRepository categoryRepository;
+
+    public List<Category> getAllCategories() {
+        return categoryRepository.findAll();
+    }
+
+    public Category createCategory(Category category) {
+        return categoryRepository.save(category);
+    }
+}
