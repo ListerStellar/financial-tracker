52e3f7ec8da918a16e0bc23bf7e4eebe17f37e9b
Kyrylo Klochek <listerstellar@gmail.com>
2026-04-23 17:12:03 +0200

    Initial commit

---
+++ src/main/java/com/finance_tracker/entity/Transaction.java
@@ -0,0 +1,32 @@
+package com.finance_tracker.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "transactions")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Transaction {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private BigDecimal amount;
+
+    @Column(nullable = false)
+    private LocalDateTime date;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "category_id", nullable = false)
+    private Category category;
+
+    private String description;
+}
